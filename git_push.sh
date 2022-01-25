b1765083722c3d89dd21401472ca4bd4decf6769
MD. SABBIR AHMED <sabbir206314@gmail.com>
2022-01-25 20:03:58 +0600

    git push added

---
+++ git_push.sh
@@ -0,0 +1,15 @@
+git add .
+
+echo 'Hello Sabbir'
+echo 'Welcome to EEE2105'
+echo 'Enter your commit message:'
+read commitMessage
+
+git commit -m "$commitMessage"
+
+git push 
+git push 
+
+echo 'Your EEE2105 Successfully Updated'
+
+read
